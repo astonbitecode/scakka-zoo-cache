9834cc96612536553e5a939c432d33aed4831236
aston <astonbitecode@gmail.com>
2016-09-12 11:45:06 +0300

    Added getDAta unit tests

--- src/test/scala/com/github/astonbitecode/ScakkaZooCacheSpec.scala
+++ src/test/scala/com/github/astonbitecode/ScakkaZooCacheSpec.scala
@@ -12,24 +12,49 @@ class ScakkaZooCacheSpec extends mutable.Specification with Mockito {
 
   "Tests for the ScakkaZooCache ".txt
 
-  "Get Children on a non-existing node should " >> {
-    "fail" >> {
-      val instance = ScakkaZooCache(zk)
-      instance.getChildren("/") must throwA[KeeperException]
+  "Get Children " >> {
+    "on a non-existing node should " >> {
+      "fail" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.getChildren("/") must throwA[KeeperException]
+      }
+    }
+
+    " on a an existing node " >> {
+      "with no children should return an empty List" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("v".getBytes))
+        instance.getChildren("/path") must haveSize(0)
+      }
+
+      "with children should return a non empty List" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("v".getBytes, Set("child1", "child2")))
+        instance.getChildren("/path") must haveSize(2)
+      }
     }
   }
 
-  "Get Children on a an existing node " >> {
-    "with no children should return an empty List" >> {
-      val instance = ScakkaZooCache(zk)
-      instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("v".getBytes))
-      instance.getChildren("/path") must haveSize(0)
+  "Get Data " >> {
+    "on a non-existing node should " >> {
+      "fail" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.getData("/") must throwA[KeeperException]
+      }
     }
 
-    "with children should return a non empty List" >> {
-      val instance = ScakkaZooCache(zk)
-      instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("v".getBytes, Set("child1", "child2")))
-      instance.getChildren("/path") must haveSize(2)
+    " on a an existing node " >> {
+      "with no data should return an empty result" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("".getBytes))
+        instance.getData("/path") must equalTo("".getBytes)
+      }
+
+      "with data should return the data of the node" >> {
+        val instance = ScakkaZooCache(zk)
+        instance.cache.put("/path", ScakkaZooCache.ZkNodeElement("v".getBytes, Set("child1", "child2")))
+        instance.getData("/path") must equalTo("v".getBytes)
+      }
     }
   }
 
