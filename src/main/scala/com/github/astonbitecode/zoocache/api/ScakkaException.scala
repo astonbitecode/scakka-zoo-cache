b8cf003e20b5dfbb6e87e191535cc5a9b66bfc0d
aston <astonbitecode@gmail.com>
2016-09-14 13:34:05 +0300

    Fixed misspell

--- src/main/scala/com/github/astonbitecode/zoocache/api/ScakkaException.scala
+++ src/main/scala/com/github/astonbitecode/zoocache/api/ScakkaException.scala
@@ -4,7 +4,7 @@ package com.github.astonbitecode.zoocache.api
  * General exception thrown by the scakka-zoo-cache.
  * There are more specific exceptions that are all derived from this one.
  */
-class ScakkaExcaption(message: String) extends Exception(message) {
+class ScakkaException(message: String) extends Exception(message) {
   def this() = this("")
 }
 
@@ -15,5 +15,5 @@ object ScakkaException {
    * - The cache is not instructed to cache the specific path or one of its parents
    * - Even if the path is added in the cache, it does not exist in the ZooKeeper
    */
-  class NotCachedException(message: String) extends ScakkaExcaption(message)
+  class NotCachedException(message: String) extends ScakkaException(message)
 }
